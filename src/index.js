15e887850fb19576e1f75f86151e68638f135b67
Tomas Holtz <tomiholtz@gmail.com>
2022-08-20 14:56:23 -0300

    Home

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter, Routes, Route } from "react-router-dom"; import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { getLibrary } from "./config/web3";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+      <ChakraProvider>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </Web3ReactProvider>
+      </ChakraProvider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
