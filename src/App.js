6c6733dbf59fe17d2a429c5eee5ade6e76779bd9
Tomas Holtz <tomiholtz@gmail.com>
2022-08-23 00:27:44 -0300

    Punk page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
 import MainLayout from "./layouts/main";
 import Punks from "./views/punks";
+import Punk from "./views/punk";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/punks" element={<Punks />} />
+        <Route path="/punks/:tokenId" element={<Punk />} />
       </Routes>
     </MainLayout>
   );
